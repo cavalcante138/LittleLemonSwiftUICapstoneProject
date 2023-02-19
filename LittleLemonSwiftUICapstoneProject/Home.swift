20d8991a64806dacd3e3262adb9ab8854638dfde
cavalcante138 <35882630+cavalcante138@users.noreply.github.com>
2023-02-19 04:47:16 -0300

    Creating the login and navigation basics

---
+++ LittleLemonSwiftUICapstoneProject/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  LittleLemonSwiftUICapstoneProject
+//
+//  Created by Lucas Cavalcante on 19/02/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ LittleLemonSwiftUICapstoneProject/UserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfile.swift
+//  LittleLemonSwiftUICapstoneProject
+//
+//  Created by Lucas Cavalcante on 19/02/23.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
