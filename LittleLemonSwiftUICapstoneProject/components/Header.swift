f9947d3f3c6f9f41634d24326be7cc4f001236ec
cavalcante138 <35882630+cavalcante138@users.noreply.github.com>
2023-02-20 07:08:31 -0300

    Completed the ui

---
+++ LittleLemonSwiftUICapstoneProject/components/Header.swift
@@ -0,0 +1,20 @@
+//
+//  Header.swift
+//  LittleLemonSwiftUICapstoneProject
+//
+//  Created by Lucas Cavalcante on 20/02/23.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Header_Previews: PreviewProvider {
+    static var previews: some View {
+        Header()
+    }
+}
